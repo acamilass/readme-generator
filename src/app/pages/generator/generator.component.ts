ae4e42ea52b5f755c8dccfa47ab9399c895043aa
Ana Camila Santos Sousa <acamilass@gmail.com>
2021-08-22 16:13:08 -0300

    Gerar arquivo para download

--- src/app/pages/generator/generator.component.ts
+++ src/app/pages/generator/generator.component.ts
@@ -9,7 +9,7 @@ import SectionModel from '../models/section';
 export class GeneratorComponent implements OnInit {
 
   titleH1!: string;
-  titleH2!: string;
+  title!: string;
   description!: string;
 
   sections!: Array<any>;
@@ -18,7 +18,7 @@ export class GeneratorComponent implements OnInit {
 
   ngOnInit(): void {
     this.sections = [
-      new SectionModel(this.titleH2, this.description)
+      new SectionModel(this.title, this.description)
     ];
   }
 
@@ -26,6 +26,45 @@ export class GeneratorComponent implements OnInit {
     let title = '';
     let description = '';
     return this.sections.push(new SectionModel(title, description));
+  }
+
+  saveTextAsFile(data: string, fileName: string) {
+
+    if (!data) {
+      console.error('Console.save: No data')
+      return;
+    }
+
+    if (!fileName) fileName = 'console.json'
+
+    let blob = new Blob([data], { type: 'text/plain' });
+    let  el = document.createEvent('MouseEvents');
+    let  a = document.createElement('a');
+
+    // FOR IE:
+
+    if (window.navigator && window.navigator.msSaveOrOpenBlob) {
+      window.navigator.msSaveOrOpenBlob(blob, fileName);
+    }
+    else {
+      el = document.createEvent('MouseEvents'),
+      a = document.createElement('a');
+
+      a.download = fileName;
+      a.href = window.URL.createObjectURL(blob);
+      a.dataset.downloadurl = ['text/plain', a.download, a.href].join(':');
+      el.initEvent('click', true, false);
+      a.dispatchEvent(el);
+    }
+  }
+
+
+  downloadFile() {
+    const fileText =
+    `# ${this.titleH1}\r\n\n## ${this.sections[0].title}\r\n${this.sections[0].description}`;
+
+    const fileName = 'README.txt';
+    this.saveTextAsFile(fileText, fileName);
   }
 
 }
