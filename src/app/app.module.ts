4b77c76580e79e2b86448493e7cd76c37e1729f5
Ana Camila Santos Sousa <acamilass@gmail.com>
2021-08-22 12:27:06 -0300

    Criar componente principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
+import { GeneratorComponent } from './pages/generator/generator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    GeneratorComponent,
   ],
   imports: [
     BrowserModule,
