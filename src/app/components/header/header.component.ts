6ebf49c59d3b582bbbe1dea21d84e04f5720f87b
Ana Camila Santos Sousa <acamilass@gmail.com>
2021-08-22 17:18:27 -0300

    Criar dark mode

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,4 +12,20 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  goToLightMode() {
+    document.getElementById('body').style.backgroundColor = '#f7f7ff';
+    document.getElementById('body').style.color = '#070600';
+    document.getElementById('section-btn').style.color = '#070600';
+    document.getElementById('btn-download').style.backgroundColor = '#435058';
+    document.getElementById('btn-download').style.color = '#f7f7ff';
+  }
+
+  goToDarkMode() {
+    document.getElementById('body').style.backgroundColor = '#1e1e24';
+    document.getElementById('body').style.color = '#f7f7ff';
+    document.getElementById('section-btn').style.color = '#f7f7ff';
+    document.getElementById('btn-download').style.backgroundColor = '#f7f7ff';
+    document.getElementById('btn-download').style.color = '#070600';
+  }
+
 }
